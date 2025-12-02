0a2a6df12aaf60f5d25df6e50184e6df5778b308
bartek3058 <bartek3058@gmail.com>
2025-12-02 19:12:27 +0100

    ex01

---
+++ ex01/ClapTrap.cpp
@@ -0,0 +1,70 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name)
+{
+    std::cout<<"ClapTrap "<<name<<" created !"<<std::endl;
+    this->Name = name;
+    this->HitPoints = 10;
+    this->EnergyPoints = 10;
+    this->AttackDamage = 0;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &settings)
+{
+    std::cout<<"Copy constructor called"<<std::endl;
+    this->Name = settings.Name;
+    this->HitPoints = settings.HitPoints;
+    this->EnergyPoints = settings.EnergyPoints;
+    this->AttackDamage = settings.AttackDamage;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &settings)
+{
+    std::cout<<"Copy assignment operator called"<<std::endl;
+    if(this != &settings)
+    {
+        this->Name = settings.Name;
+        this->HitPoints = settings.HitPoints ;
+        this->EnergyPoints = settings.EnergyPoints;
+        this->AttackDamage = settings.AttackDamage;
+    }
+    return *this;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout<<"ClapTrap Destructor called"<<std::endl;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+    if(this->EnergyPoints > 0 && this->HitPoints > 0)
+    {
+        this->EnergyPoints--;
+        std::cout<<"ClapTrap "<<this->Name<<" attacks "<<target
+        <<", causing "<<this->AttackDamage<<" points of damage!"<<std::endl;
+    }
+    else
+        std::cout << "ClapTrap " << this->Name << " can't attack (no energy or dead)!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if (this->HitPoints - amount < 0)
+        this->HitPoints = 0;
+    else
+        this->HitPoints -= amount;
+    std::cout<<"ClapTrap "<<this->Name<<" takes "<<amount<<" points of damage"<<std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (this->HitPoints > 0 && this->EnergyPoints > 0)
+    {
+        this->EnergyPoints--;
+        this->HitPoints += amount;
+        std::cout<<"ClapTrap "<<this->Name<<" repairs itself for "<<amount<<" HitPoints"<<std::endl;
+    }
+    else
+        std::cout << "ClapTrap " << this->Name << " can't repair (no energy or dead)!" << std::endl;
+}
---
+++ ex01/ScavTrap.cpp
@@ -0,0 +1,44 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
+{
+    this->HitPoints = 100;
+    this->EnergyPoints = 50;
+    this->AttackDamage = 20;
+    std::cout << "ScavTrap Constructor for " << this->Name << " called" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &src) : ClapTrap(src)
+{
+    std::cout << "ScavTrap Copy Constructor called" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &src)
+{
+    std::cout << "ScavTrap Assignation operator called" << std::endl;
+    if (this != &src)
+        ClapTrap::operator=(src);
+return *this;
+}
+
+ScavTrap::~ScavTrap()
+{
+    std::cout << "ScavTrap Destructor called" << std::endl;
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+    if(this->EnergyPoints > 0 && this->HitPoints > 0)
+    {
+        this->EnergyPoints--;
+        std::cout<<"ScavTrap "<<this->Name<<" attacks "<<target
+        <<", causing "<<this->AttackDamage<<" points of damage!"<<std::endl;
+    }
+    else
+        std::cout << "ScavTrap " << this->Name << " can't attack (no energy or dead)!" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+    std::cout<<"ScavTrap is now in Gate keeper mode."<<std::endl;
+}
